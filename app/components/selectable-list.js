32ec5ea00724cf05153094e0c6ef7263dc4d2ca0
Adam Lessen <lessena@gmail.com>
2017-01-02 12:11:32 -0800

    Add styling to selectable list items

--- app/components/selectable-list.js
+++ app/components/selectable-list.js
@@ -1,7 +1,34 @@
 import Ember from 'ember';
 
+const get = Ember.get;
+const set = Ember.set;
+
 export default Ember.Component.extend({
     tagName: 'ul',
+
+    classNames: ['selectable-list'],
+
+    selection: null,
+
+    list: null,
+
+    itemsToDisplay: Ember.computed('selection', 'list', function() {
+        const orgs = this.get('list');
+        console.warn('computing listToDisplay...');
+        const selection = this.get('selection');
+        if (orgs) {
+            orgs.forEach(org => {
+                if (org === selection) {
+                    set(org, 'selected', true);
+                } else {
+                    set(org, 'selected', false);
+                }
+            });
+            return orgs;
+        } else {
+            return null;
+        }
+    }),
 
     actions: {
         addNew() {
--- app/controllers/home.js
+++ app/controllers/home.js
@@ -5,4 +5,25 @@ var get = Ember.get;
 
 export default Ember.Controller.extend({
     selection: null,
+
+    organizations: null,
+
+    // orgsToDisplay: Ember.computed('selection', 'organizations', () => {
+    //     const orgs = get(this, 'organizations');
+    //     console.warn('computing orgsToDisplay...');
+    //     const selection = get(this, 'selection');
+    //     if (orgs) {
+    //         orgs.forEach(org => {
+    //             if (org === selection) {
+    //                 set(org, 'selected', true);
+    //             } else {
+    //                 set(org, 'selected', false);
+    //             }
+    //         });
+    //         return orgs;
+    //     } else {
+    //         return null;
+    //     }
+    // }),
+
 });
